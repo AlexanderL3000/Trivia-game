2d5179fc942ead90f20e6b133c2fed4b2cc47c5f
AlexanderL <al.x.300000@gmail.com>
2021-03-02 16:52:12 -0500

    Made a window

--- Trivia game/src/Main.java
+++ Trivia game/src/Main.java
@@ -3,7 +3,7 @@
 
 public class Main{
 	public static void main(String args[]) {
-		
+		new Window(2000,1000);
 		
 	}
 }
--- Trivia game/src/Window.java
+++ Trivia game/src/Window.java
@@ -1,4 +1,28 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 public class Window {
-
+	
+	public Window(int w, int h) {
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		JFrame frame = new JFrame();
+		
+		frame.pack();
+		
+		frame.setVisible(true);
+		frame.setPreferredSize(new Dimension(w, h));
+		frame.setMaximumSize(new Dimension(w, h));
+		frame.setMinimumSize(new Dimension(w, h));
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		frame.setResizable(true);
+		
+		
+		
+	}
+	
+	
+	
+		
 }
